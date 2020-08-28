0dcad894f9cebbb092687a9e7b8cacd79018f06f
milldev <milldev@iastate.edu>
2020-08-28 17:26:07 -0500

    More UI changes and a screen change for sign up add

--- Experiments/Devin/app/src/main/java/com/example/testapplication/MainActivity.java
+++ Experiments/Devin/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView signUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signUp = (TextView)findViewById(R.id.txtSignUp);
+        signUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.txtSignUp:
+
+                Intent startSignUpIntent = new Intent(this, SignUp.class);
+                startActivity(startSignUpIntent);
+                finish();
+
+                break;
+
+        }
+
     }
 }
