be45d79d3a0eaf0c6fd93218e9069710bfbbd200
milldev <milldev@iastate.edu>
2020-08-28 15:41:37 -0500

    Project Creation

--- Experiments/Devin/app/src/androidTest/java/com/example/testapplication/ExampleInstrumentedTest.java
+++ Experiments/Devin/app/src/androidTest/java/com/example/testapplication/ExampleInstrumentedTest.java
@@ -1,6 +1,7 @@
 package com.example.testapplication;
 
 import android.content.Context;
+
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
