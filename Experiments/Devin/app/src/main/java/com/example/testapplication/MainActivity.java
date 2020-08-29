1fb1e9edaacbc3c12ec4baa6d9821b04c9555e49
milldev <milldev@iastate.edu>
2020-08-29 13:39:46 -0500

    Sign on and up functionality and robust

--- Experiments/Devin/app/src/main/java/com/example/testapplication/MainActivity.java
+++ Experiments/Devin/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView signUp;
+    private EditText edEmail, edPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         signUp = (TextView)findViewById(R.id.txtSignUp);
         signUp.setOnClickListener(this);
+
+        edEmail = (EditText)findViewById(R.id.edEmail);
+        edPass = (EditText)findViewById(R.id.edPass);
+
     }
+
 
     @Override
     public void onClick(View v) {
@@ -26,6 +33,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()){
 
             case R.id.txtSignUp:
+
+                String email = edEmail.getText().toString().trim();
+                String pass = edPass.getText().toString().trim();
+
+                if(email.isEmpty()){
+                    break;
+                }
+                if(pass.isEmpty()){
+                    break;
+                }
 
                 Intent startSignUpIntent = new Intent(this, SignUp.class);
                 startActivity(startSignUpIntent);
--- Experiments/Devin/app/src/main/java/com/example/testapplication/SignUp.java
+++ Experiments/Devin/app/src/main/java/com/example/testapplication/SignUp.java
@@ -3,27 +3,39 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
-    private TextView signIn;
+    private TextView signIn, txtRePass, txtPass, txtReEmail;
     private Button btnSignUp;
+
+    private EditText edEmail, edPassword, edRePass, edAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        txtPass = (TextView)findViewById(R.id.txtRePass);
+        txtReEmail = (TextView)findViewById(R.id.txtReEm);
+        txtRePass = (TextView)findViewById(R.id.txtReRePass);
         signIn = (TextView)findViewById(R.id.txtSignIn);
         signIn.setOnClickListener(this);
 
         btnSignUp = (Button)findViewById(R.id.btnSignUp);
         btnSignUp.setOnClickListener(this);
+
+        edAddress = (EditText)findViewById(R.id.edAddress);
+        edEmail = (EditText)findViewById(R.id.edEmail);
+        edPassword = (EditText)findViewById(R.id.edPassSign);
+        edRePass = (EditText)findViewById(R.id.edPass2Sign);
     }
 
     @Override
@@ -40,6 +52,52 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
                 break;
 
             case R.id.btnSignUp:
+
+                boolean check = false;
+
+                String rePass = edRePass.getText().toString().trim();
+                String pass = edPassword.getText().toString().trim();
+                String email = edEmail.getText().toString().trim();
+                String add = edAddress.getText().toString();
+                if(add.isEmpty()){
+
+                }
+                if(rePass.isEmpty()){
+
+                    txtRePass.setTextColor(Color.RED);
+                    check = true;
+                }else{
+                    txtRePass.setTextColor(getResources().getColor(R.color.AccentText));
+                }
+                if(pass.isEmpty()){
+
+                    txtPass.setTextColor(Color.RED);
+                    check = true;
+                }else {
+                    txtPass.setTextColor(getResources().getColor(R.color.AccentText));
+                }
+                if(!check && !rePass.equals(pass)){
+
+                    txtPass.setTextColor(Color.RED);
+                    txtRePass.setTextColor(Color.RED);
+                    check = true;
+                }
+                else if(!check){
+                    txtRePass.setTextColor(getResources().getColor(R.color.AccentText));
+                    txtPass.setTextColor(getResources().getColor(R.color.AccentText));
+                }
+
+                if(email.isEmpty()){
+
+                    txtReEmail.setTextColor(Color.RED);
+                    check = true;
+                }else{
+                    txtReEmail.setTextColor(getResources().getColor(R.color.AccentText));
+                }
+
+                if(check){
+                    break;
+                }
 
                 Intent signUpIntent = new Intent(this, MainActivity.class);
                 startActivity(signUpIntent);
