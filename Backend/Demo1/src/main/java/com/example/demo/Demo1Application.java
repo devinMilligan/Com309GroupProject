dec5e9b32e2997a4992ee6c4295888af478affa3
Ty Wallis <tjwallis@iastate.edu>
2020-09-01 10:20:40 -0500

    Created new project and added a basic Controller

--- Backend/Demo1/src/main/java/com/example/demo/Demo1Application.java
+++ Backend/Demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Demo1Application {
 
-    public static void main(String[] args) {
-
-        SpringApplication.run(DemoApplication.class, args);
-    }
-
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1Application.class, args);
+	}
 }
