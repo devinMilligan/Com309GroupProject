c7e7817076a300dffbde8025cce90115d922b3ff
aafinger <aafinger@iastate.edu>
2020-09-23 05:52:00 -0500

    map is working now

---
+++ Frontend/app/src/main/java/com/example/project309/app/ui/map/MarkerInfoWindowAdapter.java
@@ -0,0 +1,40 @@
+package com.example.project309.app.ui.map;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.project309.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+
+public class MarkerInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+    private Context context;
+    public MarkerInfoWindowAdapter(Context context) {
+        this.context = context.getApplicationContext();
+    }
+
+    @Override
+    public View getInfoWindow(Marker arg0) {
+        return null;
+    }
+
+    @Override
+    public View getInfoContents(Marker arg0) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v =  inflater.inflate(R.layout.map_marker_info_window, null);
+
+        LatLng latLng = arg0.getPosition();
+        TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
+        TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
+        TextView restName = (TextView) v.findViewById(R.id.rest_name);
+        TextView restHours = (TextView) v.findViewById(R.id.rest_hours);
+        tvLat.setText("Latitude:" + latLng.latitude);
+        tvLng.setText("Longitude:"+ latLng.longitude);
+        restName.setText("Dining Center: Conversations");
+        restHours.setText("Hours of Operation: 8am - 5pm");
+        return v;
+    }
+}
