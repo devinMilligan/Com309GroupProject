a8311623c77bdd22ff9bbc017f058e1e8ee60264
Ty Wallis <tjwallis@iastate.edu>
2020-10-05 14:26:09 -0500

    Edited store hour handling in StoreDao

--- Backend/309Backend/src/main/java/com/project/backend/StoreDao.java
+++ Backend/309Backend/src/main/java/com/project/backend/StoreDao.java
@@ -5,6 +5,9 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 @Repository
@@ -32,6 +35,37 @@ public class StoreDao {
         store.setPhone(resultSet.getString("PHONE"));
         store.setLatitude(resultSet.getDouble("LATITUDE"));
         store.setLongitude(resultSet.getDouble("LONGITUDE"));
+        
+        LinkedList<List<Time>> hours = new LinkedList<List<Time>>();
+        List<Time> sunHours = new ArrayList<Time>();
+        sunHours.add(resultSet.getTime("OPENS_SUNDAY"));
+        sunHours.add(resultSet.getTime("CLOSES_SUNDAY"));
+        List<Time> monHours = new ArrayList<Time>();
+        monHours.add(resultSet.getTime("OPENS_MONDAY"));
+        monHours.add(resultSet.getTime("CLOSES_MONDAY"));
+        List<Time> tuesHours = new ArrayList<Time>();
+        tuesHours.add(resultSet.getTime("OPENS_TUESDAY"));
+        tuesHours.add(resultSet.getTime("CLOSES_TUESDAY"));
+        List<Time> wedHours = new ArrayList<Time>();
+        wedHours.add(resultSet.getTime("OPENS_WEDNESDAY"));
+        wedHours.add(resultSet.getTime("CLOSES_WEDNESDAY"));
+        List<Time> thursHours = new ArrayList<Time>();
+        thursHours.add(resultSet.getTime("OPENS_THURSDAY"));
+        thursHours.add(resultSet.getTime("CLOSES_THURSDAY"));
+        List<Time> friHours = new ArrayList<Time>();
+        friHours.add(resultSet.getTime("OPENS_FRIDAY"));
+        friHours.add(resultSet.getTime("CLOSES_FRIDAY"));
+        List<Time> satHours = new ArrayList<Time>();
+        satHours.add(resultSet.getTime("OPENS_SATURDAY"));
+        satHours.add(resultSet.getTime("CLOSES_SATURDAY"));
+        hours.add(sunHours);
+        hours.add(monHours);
+        hours.add(tuesHours);
+        hours.add(thursHours);
+        hours.add(friHours);
+        hours.add(satHours);        
+        store.setHours(hours);
+        
         return store;
     }
 }
