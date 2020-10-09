d609e0f5c763abb18b6dbac71d69d2eae3a23225
Ryan Hickok <rphickok@gmail.com>
2020-10-08 19:56:25 -0500

    Minor changes before merge

--- Frontend/app/src/main/java/com/example/project309/app/LoginActivity.java
+++ Frontend/app/src/main/java/com/example/project309/app/LoginActivity.java
@@ -115,8 +115,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             for(int i = 0; i < response.length(); i++) {
                 try {
                     s = response.get(i).toString();
-                    Intent loggedIn = new Intent(LoginActivity.this, MainNavigationScreen.class);
-                    startActivity(loggedIn);
+                    if(email.getText().toString().trim().equals("Admin")){
+                        Intent loggedIn = new Intent(this, MainNavigationScreenAdmin.class);
+                        startActivity(loggedIn);
+
+                    }else if(email.getText().toString().trim().equals("Store")) {
+                        Intent loggedIn = new Intent(this, MainNavigationScreenStore.class);
+                        startActivity(loggedIn);
+
+                    }else {
+                        Intent loggedIn = new Intent(this, MainNavigationScreen.class);
+                        startActivity(loggedIn);
+
+                    }
                     return;
                 }
                 catch(JSONException e) {
