8b3b17ed08986ed18ff466ed6249d0895a1d1454
Ty Wallis <tjwallis@iastate.edu>
2020-10-16 15:59:08 -0500

    Stopped testing script

--- Backend/309Backend/src/test/java/com/example/demo/ApplicationTests.java
+++ Backend/309Backend/src/test/java/com/example/demo/ApplicationTests.java
@@ -30,59 +30,19 @@ public class ApplicationTests {
 	@Test
 	public void getUserInfo() {
 		
-		User exampleUser = new User();
-
-		assertEquals(123, exampleUser.getId());
-		assertEquals("email", exampleUser.getEmail());
-		assertEquals("password", exampleUser.getPassword());
-		assertEquals("address", exampleUser.getAddress());
-		assertEquals("first", exampleUser.getFirstName());
-		assertEquals("last", exampleUser.getLastName());
-		assertEquals("delivery", exampleUser.getType());
-		assertEquals("image_path", exampleUser.getImagePath());
+		
 	}
 	
 	@Test
 	public void setUserInfo() {
 		
-		User exampleUser = new User();
 		
-		exampleUser.setId(101);
-		exampleUser.setEmail("example@gmail.com");
-		exampleUser.setPassword("abc123");
-		exampleUser.setAddress("123 Main St.");
-		exampleUser.setFirstName("Joe");
-		exampleUser.setLastName("Smith");
-		exampleUser.setType("delivery");
-		exampleUser.setImagePath("C://Pictures//image.png");
-
-		assertEquals(101, exampleUser.getId());
-		assertEquals("example@gmail.com", exampleUser.getEmail());
-		assertEquals("abc123", exampleUser.getPassword());
-		assertEquals("123 Main St.", exampleUser.getAddress());
-		assertEquals("Joe", exampleUser.getFirstName());
-		assertEquals("Smith", exampleUser.getLastName());
-		assertEquals("delivery", exampleUser.getType());
-		assertEquals("C://Pictures//image.png", exampleUser.getImagePath());
 	}
 	
 	@Test
 	public void loadAllUsers() {
-		List<User> list = new ArrayList<User>();
-		User acctOne = new User();
-		User acctTwo = new User();
-		User acctThree = new User();
-
-		list.add(acctOne);
-		list.add(acctTwo);
-		list.add(acctThree);
 		
-		when(dao.loadAll()).thenReturn(list);
-
-		List<User> users = dao.loadAll();
-
-		assertEquals(3, users.size());
-		verify(dao, times(1)).loadAll();
+		
 	}
 	
 	
