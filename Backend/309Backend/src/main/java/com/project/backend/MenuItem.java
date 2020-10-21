28bcea9ff81aede006e9365a59f9eff03501aeb5
Ty Wallis <tjwallis@iastate.edu>
2020-10-21 15:44:03 -0500

    Completed MenuItem class

--- Backend/309Backend/src/main/java/com/project/backend/MenuItem.java
+++ Backend/309Backend/src/main/java/com/project/backend/MenuItem.java
@@ -10,6 +10,49 @@ import javax.persistence.Table;
 @Table(name = "MenuItems")
 public class MenuItem {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
 	
+	private String name;
+	private double price;
+	private String description;
+	private int store;
 	
+
+    public int getId() {
+        return id;
+    }    
+    public void setId(int id) {
+        this.id = id;
+    }
+	
+    public String getName() {
+    	return this.name;
+    }
+    public void setName(String name) {
+    	this.name = name;
+    }
+	
+    public double getPrice() {
+    	return this.price;
+    }
+    public void setPrice(double price) {
+    	this.price = price;
+    }
+	
+    public String getDescription() {
+    	return this.description;
+    }
+    public void setDescription(String description) {
+    	this.description = description;
+    }
+    
+    public int getStore() {
+        return store;
+    }    
+    public void setStore(int store) {
+        this.store = store;
+    }
+    
 }
