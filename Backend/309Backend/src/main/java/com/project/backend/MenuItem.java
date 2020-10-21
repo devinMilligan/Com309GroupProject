c45c496968127a250bf98476981fa498846e020e
Ty Wallis <tjwallis@iastate.edu>
2020-10-21 15:03:35 -0500

    Created class for MenuItems

---
+++ Backend/309Backend/src/main/java/com/project/backend/MenuItem.java
@@ -0,0 +1,15 @@
+package com.project.backend;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "MenuItems")
+public class MenuItem {
+
+	
+	
+}
