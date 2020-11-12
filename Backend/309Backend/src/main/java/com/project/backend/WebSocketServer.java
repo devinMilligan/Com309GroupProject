8a7a1b6ea41d878f20a93638ea4001a52cc6528a
Ty Wallis <tjwallis@iastate.edu>
2020-11-12 15:38:29 -0600

    Complete WebSocket class

---
+++ Backend/309Backend/src/main/java/com/project/backend/WebSocketServer.java
@@ -0,0 +1,83 @@
+package com.project.backend;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+/**
+ * 
+ * @author Vamsi Krishna Calpakkam
+ *
+ */
+@ServerEndpoint("/websocket/{username}")
+@Component
+public class WebSocketServer {
+	
+	// Store all socket session and their corresponding username.
+    private static Map<Session, String> sessionUsernameMap = new HashMap<>();
+    private static Map<String, Session> usernameSessionMap = new HashMap<>();
+    
+    private final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+    
+    @OnOpen
+    public void onOpen(
+    	      Session session, 
+    	      @PathParam("username") String username) throws IOException 
+    {
+        logger.info("Entered into Open");
+        
+        sessionUsernameMap.put(session, username);
+        usernameSessionMap.put(username, session);		
+    }
+ 
+    @OnMessage
+    public void onMessage(Session session, String message) throws IOException 
+    {
+        // Handle new messages
+    	logger.info("Entered into Message: Got Message:"+message);
+    	String username = sessionUsernameMap.get(session);
+    	
+    	if (message.startsWith("@")) // Direct message to a user using the format "@username <message>"
+    	{
+    		String destUsername = message.split(" ")[0].substring(1); // don't do this in your code!
+    		sendMessageToPArticularUser(destUsername, "[DM] " + username + ": " + message);
+    	}
+    }
+ 
+    @OnClose
+    public void onClose(Session session) throws IOException
+    {
+    	logger.info("Entered into Close");
+    	
+    	String username = sessionUsernameMap.get(session);
+    	sessionUsernameMap.remove(session);
+    	usernameSessionMap.remove(username);
+    }
+ 
+    @OnError
+    public void onError(Session session, Throwable throwable) 
+    {
+    	logger.info("Entered into Error");
+    }
+    
+	private void sendMessageToPArticularUser(String username, String message) 
+    {	
+    	try {
+    		usernameSessionMap.get(username).getBasicRemote().sendText(message);
+        } catch (IOException e) {
+        	logger.info("Exception: " + e.getMessage().toString());
+            e.printStackTrace();
+        }
+    }
+}
