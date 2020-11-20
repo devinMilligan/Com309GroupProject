d0de59bbeef270ca9a6d63f01ff1be12d1a86b61
Ty Wallis <tjwallis@iastate.edu>
2020-11-20 00:58:35 -0600

    Revert to old commit

--- Backend/309Backend/src/main/java/com/project/backend/WebSocketServer.java
+++ Backend/309Backend/src/main/java/com/project/backend/WebSocketServer.java
@@ -1,7 +1,7 @@
 package com.project.backend;
 
 import java.io.IOException;
-import java.util.Hashtable;
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.websocket.OnClose;
@@ -15,91 +15,67 @@ import javax.websocket.server.ServerEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 
 @ServerEndpoint("/websocket/{username}")
-@Component
+@Controller
 public class WebSocketServer {
-
+	
 	// Store all socket session and their corresponding username.
-    private static Map<Session, String> sessionUsernameMap = new Hashtable<>();
-    private static Map<String, Session> usernameSessionMap = new Hashtable<>();
-
+    private static Map<Session, String> sessionUsernameMap = new HashMap<>();
+    private static Map<String, Session> usernameSessionMap = new HashMap<>();
+    
     private final Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
-
+    
     @OnOpen
-    public void onOpen( Session session, @PathParam("username") String username)
-						throws IOException
+    public void onOpen(
+    	      Session session, 
+    	      @PathParam("username") String username) throws IOException 
     {
         logger.info("Entered into Open");
-
+        
         sessionUsernameMap.put(session, username);
-        usernameSessionMap.put(username, session);
-
-        String message="User:" + username + " has Joined the Chat";
-        	broadcast(message);
+        usernameSessionMap.put(username, session);		
     }
-
+ 
     @OnMessage
-    public void onMessage(Session session, String message) throws IOException
+    public void onMessage(Session session, String message) throws IOException 
     {
         // Handle new messages
     	logger.info("Entered into Message: Got Message:"+message);
     	String username = sessionUsernameMap.get(session);
-
-    	if (message.startsWith("@")) // Direct message to a user using the format "@username <message>"
+    	
+    	if (message.startsWith("@")) // Direct message to a user using the format  "@username <message>"
     	{
-    		String destUsername = message.split(" ")[0].substring(1); // don't do this in your code!
+    		String destUsername= message.substring(1, message.indexOf(" "));
     		sendMessageToPArticularUser(destUsername, "[DM] " + username + ": " + message);
-    		sendMessageToPArticularUser(username, "[DM] " + username + ": " + message);
-    	}
-    	else // Message to whole chat
-    	{
-	    	broadcast(username + ": " + message);
     	}
     }
-
+ 
     @OnClose
     public void onClose(Session session) throws IOException
     {
     	logger.info("Entered into Close");
-
+    	
     	String username = sessionUsernameMap.get(session);
     	sessionUsernameMap.remove(session);
     	usernameSessionMap.remove(username);
-
-    	String message= username + " disconnected";
-        broadcast(message);
     }
-
+ 
     @OnError
-    public void onError(Session session, Throwable throwable)
+    public void onError(Session session, Throwable throwable) 
     {
-        // Do error handling here
     	logger.info("Entered into Error");
     }
-
-	  private void sendMessageToPArticularUser(String username, String message)
-    {
-    	  try {
-    		  usernameSessionMap.get(username).getBasicRemote().sendText(message);
+    
+	private void sendMessageToPArticularUser(String username, String message) 
+    {	
+    	try {
+    		usernameSessionMap.get(username).getBasicRemote().sendText(message);
         } catch (IOException e) {
         	logger.info("Exception: " + e.getMessage().toString());
-          e.printStackTrace();
+            e.printStackTrace();
         }
-    }
-
-    private void broadcast(String message)
-		{
-			  sessionUsernameMap.forEach((session, username)->{
-					try {
-								session.getBasicRemote().sendText(message);
-					} catch (IOException e) {
-								logger.info("Exception: " + e.getMessage().toString());
-								e.printStackTrace();
-					}
-
-				});
-
     }
 }
